4c1e33008cd8d68bbb590ea9bd3cab9dea31fc45
saswat-thoughtclan <saswat.kumar.panda@thoughtclan.com>
2025-01-08 11:06:02 +0530

    added circuit breker for fault tolerance

--- CustomerService/src/main/java/com/customer/controller/CustomerController.java
+++ CustomerService/src/main/java/com/customer/controller/CustomerController.java
@@ -5,9 +5,11 @@ import com.customer.entity.Booking;
 import com.customer.entity.Customer;
 import com.customer.entity.CustomerBooking;
 import com.customer.repository.CustomerRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -30,10 +32,15 @@ public class CustomerController {
     }
 
     @GetMapping(path = "/customer-bookings/{customerId}")
+    @CircuitBreaker(name = "bookingService",fallbackMethod = "getCustomerBookingsByCustomerIdFallback")
     public CustomerBooking getCustomerBookings(@PathVariable Long customerId){
         Customer customer = customerRepository.findById(customerId).get();
         List<Booking> bookingList = bookingClient.getCustomerBookingsByCustomerId(customerId);
         return new CustomerBooking(customer,bookingList);
+    }
+    public CustomerBooking getCustomerBookingsByCustomerIdFallback(Long customerId, Throwable throwable){
+        System.out.println("service is down , err:"+throwable.getMessage());
+        return new CustomerBooking(null, new ArrayList<>());
     }
 
 
